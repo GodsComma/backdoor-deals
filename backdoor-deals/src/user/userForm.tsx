0d7d1157ec6188cdb59ba27345cce6d88a02b965
Helena Gray <anaisgray.ag@gmail.com>
2023-05-01 23:28:07 -0400

    Update userForm.tsx

--- backdoor-deals/src/user/userForm.tsx
+++ backdoor-deals/src/user/userForm.tsx
@@ -1,23 +1,32 @@
-import { useState } from "react"
+import { useState } from "react";
 
+// Create an object to store user data
 const UserData = {
     name: '',
     regTime: ''
-}
+};
 
+// Create a React component called UserForm
 export const UserForm = () => {
-    const [name, setName] = useState('')
 
+    // Initialize the "name" state variable to an empty string
+    const [name, setName] = useState('');
+
+    // Define a function that saves the user's name and the current time to local storage and redirects the user to the "/lobby/abs" page
     const saveData = () => {
+        // Update the UserData object with the user's name and the current time
         UserData.name = name;
         UserData.regTime = new Date().toString();
+        // Store the UserData object in local storage as a JSON string
         localStorage.setItem('UserData', JSON.stringify(UserData));
+        // Redirect the user to the "/lobby/abs" page
         window.location.replace('/lobby/abs');
-        return
+        return;
     }
 
+    // Render the user form
     return (
-        <div className=" mx-auto w-full max-w-xs z-10" >
+        <div className="mx-auto w-full max-w-xs z-10">
             <div className="bg-teal-200 shadow-md rounded-3xl px-8 pt-6 pb-8 mb-4" id="form">
                 <div className="mb-4">
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
@@ -28,11 +37,12 @@ export const UserForm = () => {
                         type="text"
                         name="username"
                         placeholder="Name"
+                        // Update the "name" state variable whenever the user types in the input field
                         onChange={(e) => setName(e.target.value)}
                     />
                 </div>
                 <div className="flex items-center justify-between">
-                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" onClick={()=> saveData()}>
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" onClick={() => saveData()}>
                         Save
                     </button>
                 </div>
@@ -42,7 +52,8 @@ export const UserForm = () => {
                 </p>
             </div>
         </div>
-    )
+    );
 }
 
+// Export the UserForm component as the default export
 export default UserForm;
