a84d4a8ce3ad0aa502dd646fa168638c7d619eb8
Helena Gray <anaisgray.ag@gmail.com>
2023-05-01 23:19:32 -0400

    Update chatReducer.tsx

--- backdoor-deals/src/contexts/chatReducer.tsx
+++ backdoor-deals/src/contexts/chatReducer.tsx
@@ -1,16 +1,16 @@
-/* eslint-disable */
-
+// Define the type of User
 export type User = {
     name: string
 }
 
+// Define the type of Chat
 export type Chat = {
     data: string,
     user: User,
     timestamp: string
 }
 
-
+// Define the function that handles sending a message
 function handleChatSend(action: any){
     return {
         data: action.data,
@@ -19,6 +19,7 @@ function handleChatSend(action: any){
     }
 }
 
+// Define the function that handles receiving a message
 function handleChatRecieve(action: any){
     return {
         data: action.data,
@@ -27,23 +28,30 @@ function handleChatRecieve(action: any){
     }
 }
 
+// Define constants for the action types
 export const ACTION_TYPES = {
     SEND_MESSAGE: 'send',
     RECIEVE_MESSAGE: 'recieve'
 }
 
+// Define the reducer function for the chat
 export const ChatReducer = (state: Chat[], action: any) => {
     switch (action.type) {
+        // If the action type is sending a message
         case ACTION_TYPES.SEND_MESSAGE:
+            // Return a new state with the new message added to the end
             return [
             ...state,
             handleChatSend(action)
             ];
+        // If the action type is receiving a message
         case ACTION_TYPES.RECIEVE_MESSAGE:
+            // Return a new state with the new message added to the end
             return [
             ...state,
             handleChatRecieve(action)
             ];
+        // If the action type is not recognized, return the current state
         default:
             return state
     }
